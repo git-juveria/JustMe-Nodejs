ef3415d55cad6b77a3059e893467a5400e998901
Juveria Manzar <juveriadot@gmail.com>
2020-04-12 14:19:58 +0530

    Project Setup

---
+++ mvc/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+const indexCtrl = require('../controllers/index')
+
+/* GET home page. */
+router.get('/', indexCtrl.getHomePage)
+
+module.exports = router;
